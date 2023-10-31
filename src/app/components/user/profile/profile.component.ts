48cbabf600d48df8712bc4594a79d8ea5da223db
Arnab Goswami <79049651+arnab64g@users.noreply.github.com>
2023-10-31 15:24:54 +0600

    Title Set Dynamic

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { TokenDecoded } from 'src/app/models/Result';
 import { ProfileData } from 'src/app/models/User';
@@ -12,12 +13,13 @@ import { UserService } from 'src/services/user.service';
 export class ProfileComponent implements OnInit{
   userId !: string;
   user !: ProfileData;
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private titleService : Title) {}
   
   ngOnInit(): void {
     const token = this.userService.getToken(); 
     const jwtHelper = new JwtHelperService();
     const user = jwtHelper.decodeToken<TokenDecoded>(token!);
+    this.titleService.setTitle(`PersonalBook - ${user?.actort}`)
     this.userId =String(user?.unique_name);
     this.userService.getProfailDetails(this.userId).subscribe(
       retponse => {
@@ -25,5 +27,4 @@ export class ProfileComponent implements OnInit{
       }
     );
   }
-
 }
